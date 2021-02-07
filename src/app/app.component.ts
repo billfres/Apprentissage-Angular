1e978b68add6cca7c4a7b3e32e0eb3199997a0cb
fresnel <ngomafresnel@gmail.com>
2021-02-07 23:28:35 +0100

    Ajout des services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { AppareilService } from './services/appareil.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'projetv1';
+
+  isAuth = false;
+  //lastUpdate = new Date();
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
+
+/*
+  appareilOne = 'Machine à laver';
+  appareilTwo = 'Frigo';
+  appareilThree = 'Ordinateur';*/
+
+  appareils: any[];
+
+  constructor( private appareilService : AppareilService) {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+
+  ngOnInit(){
+    this.appareils = this.appareilService.appareils;
+  }
+
+  onAllumer() {
+    this.appareilService.switchOnAll();
+  }
+  
+  onEteindre() {
+    if(confirm('Etes-vous sûr de vouloir éteindre tous vos appareils ?')) {
+      this.appareilService.switchOffAll();
+    } else {
+      return null;
+    }
+  }
 }
---
+++ src/app/appareil/appareil.component.ts
@@ -0,0 +1,47 @@
+import { AppareilService } from './../services/appareil.service';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appareil',
+  templateUrl: './appareil.component.html',
+  styleUrls: ['./appareil.component.scss']
+})
+export class AppareilComponent implements OnInit {
+
+  @Input() appareilName: string;
+  @Input() appareilStatus: string;
+  @Input() indexOfAppareil: number;
+
+  constructor( private appareilService: AppareilService) { }
+
+  ngOnInit(): void {
+  }
+
+  getStatus() {
+    return this.appareilStatus;
+  }
+
+  getColor() {
+    if(this.appareilStatus === 'allumé') {
+      return 'green';
+    } else if(this.appareilStatus === 'éteint') {
+      return 'red';
+    }
+  }
+
+  onSwitch() {
+    if(this.appareilStatus === 'allumé') {
+      this.appareilService.switchOffOne(this.indexOfAppareil);
+    } else if(this.appareilStatus === 'éteint') {
+      this.appareilService.switchOnOne(this.indexOfAppareil);
+    }
+  }
+ /* onSwitch(){
+    this.appareilService.switchOnOne(this.indexOfAppareil);
+  }
+
+  onSwitchOff(){
+    this.appareilService.switchOffOne(this.indexOfAppareil);
+  }*/
+
+}
