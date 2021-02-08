0e2edf73f304dad165728df768b5d1613552bb9d
fresnel <ngomafresnel@gmail.com>
2021-02-08 03:11:36 +0100

    Ajout des routes dans app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +9,14 @@ import { FormsModule } from '@angular/forms';
 import{ AppareilService } from './services/appareil.service';
 import { AuthComponent } from './auth/auth.component';
 import { AppareilViewComponent } from './appareil-view/appareil-view.component';
+import { RouterModule, Routes } from '@angular/router';
+
+//creation des routes
+const appRoutes: Routes = [
+  { path: 'appareils', component: AppareilViewComponent},
+  { path: 'auth', component: AuthComponent},
+  { path: '', component: AppareilViewComponent}
+];
 
 @NgModule({
   declarations: [
@@ -20,7 +28,8 @@ import { AppareilViewComponent } from './appareil-view/appareil-view.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     AppareilService
