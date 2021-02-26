70463238e435354d68c0c3fbdf00da03ff0fc6f1
fresnel <ngomafresnel@gmail.com>
2021-02-26 18:38:00 +0100

    Ajout d'un formulaire edit-apparel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,11 +14,13 @@ import { AppareilViewComponent } from './appareil-view/appareil-view.component';
 import { RouterModule, Routes } from '@angular/router';
 import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
 import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import { EditAppareilComponent } from './edit-appareil/edit-appareil.component';
 
 //creation des routes
 const appRoutes: Routes = [
   { path: 'appareils',  canActivate: [AuthGuard], component: AppareilViewComponent},
   { path: 'appareils/:id', canActivate: [AuthGuard], component: SingleAppareilComponent},
+  { path: 'edit', canActivate: [AuthGuard], component: EditAppareilComponent},
   { path: 'auth', component: AuthComponent},
   { path: '', component: AppareilViewComponent},
   { path: 'not-found', component: FourOhFourComponent},
@@ -32,7 +34,8 @@ const appRoutes: Routes = [
     AuthComponent,
     AppareilViewComponent,
     SingleAppareilComponent,
-    FourOhFourComponent
+    FourOhFourComponent,
+    EditAppareilComponent
   ],
   imports: [
     BrowserModule,
