ed7207c9a36f5fd38b6612d00a5694d32383c7ea
fresnel <ngomafresnel@gmail.com>
2021-03-16 14:52:02 +0100

    Affichage des utilisateurs avec forms methode reactive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { UserService } from './services/user.service';
 import { AuthGuard } from './services/auth-guard.service';
 import { AuthService } from './services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
@@ -6,7 +7,7 @@ import { NgModule, Component } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppareilComponent } from './appareil/appareil.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 //import de AppareilService et ajout de AppareilService dans providers
 import{ AppareilService } from './services/appareil.service';
 import { AuthComponent } from './auth/auth.component';
@@ -15,6 +16,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
 import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 import { EditAppareilComponent } from './edit-appareil/edit-appareil.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 //creation des routes
 const appRoutes: Routes = [
@@ -22,6 +24,7 @@ const appRoutes: Routes = [
   { path: 'appareils/:id', canActivate: [AuthGuard], component: SingleAppareilComponent},
   { path: 'edit', canActivate: [AuthGuard], component: EditAppareilComponent},
   { path: 'auth', component: AuthComponent},
+  { path: 'users', component: UserListComponent },
   { path: '', component: AppareilViewComponent},
   { path: 'not-found', component: FourOhFourComponent},
   { path: '**', redirectTo: 'not-found' }
@@ -35,17 +38,20 @@ const appRoutes: Routes = [
     AppareilViewComponent,
     SingleAppareilComponent,
     FourOhFourComponent,
-    EditAppareilComponent
+    EditAppareilComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [
     AppareilService,
     AuthService,
+    UserService,
     AuthGuard
   ],
   bootstrap: [AppComponent]
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,7 +2,13 @@ import { Subject } from 'rxjs/Subject';
 import { User } from "../models/User.model";
 
 export class UserService{
+   /*
     private users: User[] = [];
+    cet array vide sera remplacé par le tableau en bas
+  */
+ private users: User[] = [
+    new User('Will', 'Alexander', 'will@will.com', 'jus d\'orange', ['coder', 'boire du café'])
+  ];
     userSubject = new Subject<User[]>();
 
     emitUsers(){
